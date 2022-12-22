ca98f7d17f133c912f56f13f81b5e8138bc576a4
Louis Wilbrink <louis.wilbrink@gmail.com>
2022-12-21 21:49:58 -0500

    * Added routine to retrieve link_token from Plaid. * Added `serde` crate for serialization.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 use dotenv::dotenv;
 use std::env;
 
-use std::error::Error;
 use std::collections::HashMap;
 
 use env::var;
 use env::VarError;
 
-// Types
+//use serde::ser::{Serialize, Serializer, SerializeStruct};
+use serde::{Serialize, Deserialize};
+
+
 #[derive(Debug)]
 pub struct Credentials {
     pub client_id: String,
@@ -30,6 +32,13 @@ pub struct Transaction {
 }
 
 #[derive(Debug)]
+pub struct LinkToken {
+    expiration: String,
+    link_token: String,
+    request_id: String
+}
+
+#[derive(Debug)]
 pub enum Category {
     Groceries,
     EatingOut,
@@ -42,7 +51,7 @@ pub fn load_configuration() -> Configuration {
 
     match config {
         Ok(config) => config,
-        Err(var_error) => panic!("Missing environment variables in the `.env` file.")
+        Err(_) => panic!("Missing environment variables in the `.env` file.")
     }
 }
 
@@ -59,11 +68,61 @@ fn read_environment_variables() -> Result<Configuration, VarError> {
     })
 }
 
-pub async fn get_link_token(credentials: &Credentials) -> Result<HashMap<String, String>, Box<dyn Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
+pub async fn get_link_token(config: &Configuration) -> LinkToken {
+    let link_token = match request_link_token(&config).await {
+        Ok(link_token) => link_token,
+        Err(error) => panic!("Error requesting link token: {:?}", error)
+    };
+
+    link_token
+}
+
+async fn request_link_token(config: &Configuration) -> Result<LinkToken, reqwest::Error> {
+    let url = format!("{}/{}", config.environment_url, "link/token/create");
+
+    let client = reqwest::Client::new();
+
+    let link_token_request_body = LinkTokenRequestBody {
+        client_id: &config.credentials.client_id,
+        secret: &config.credentials.client_secret,
+        client_name: String::from("Insert Client name here"),
+        country_codes: [String::from("US")],
+        language: String::from("en"),
+        user: LinkTokenUser {
+            client_user_id: String::from("unique_user_id")
+        },
+        products: [String::from("auth")]
+    };
+
+    let resp = client.post(url)
+        .json(&link_token_request_body)
+        .send()
         .await?
         .json::<HashMap<String, String>>()
         .await?;
+
+    let link_token = LinkToken {
+        expiration: resp.get("expiration").unwrap().to_string(),
+        link_token: resp.get("link_token").unwrap().to_string(),
+        request_id: resp.get("request_id").unwrap().to_string()
+    };
     
-    Ok(resp)
+    Ok(link_token)
+
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct LinkTokenRequestBody<'a> {
+    client_id: &'a str,
+    secret: &'a str,
+    client_name: String,
+    country_codes: [String; 1],
+    language: String,
+    user: LinkTokenUser,
+    products: [String; 1]
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct LinkTokenUser {
+    client_user_id: String
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,15 @@
 use std::error::Error;
 
-use hypertx::Credentials;
-use hypertx::Configuration;
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let config = hypertx::load_configuration();
 
     // Obtain a link_token by calling /link/token/create.
-    match hypertx::get_link_token(&config.credentials).await {
-        Ok(link) => println!("Worked! {:?}", link),
-        Err(error) => println!("Doh!"),
-    }
+    let link_token = hypertx::get_link_token(&config).await;
 
-    println!("Config: {:?}", config);
+    println!("LinkToken: {:?}", link_token);
 
     // Get public_token, exchange for access_token.
     
-     
     Ok(())
 }
